a73cec81986289c1ec5f33cab5e86e33f139b730
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2016-12-13 21:57:58 +0100

    Enable test for fifth day

--- src/D5/Tests/AdvancedCodeCrackerTest.php
+++ src/D5/Tests/AdvancedCodeCrackerTest.php
@@ -8,7 +8,7 @@ class AdvancedCodeCrackerTest extends \PHPUnit_Framework_TestCase
 {
     public function test_code_cracker()
     {
-//        $cracker = new AdvancedCodeCracker('abc');
-//        $this->assertEquals('05ace8e3', $cracker->getCode());
+        $cracker = new AdvancedCodeCracker('abc');
+        $this->assertEquals('05ace8e3', $cracker->getCode());
     }
 }
--- src/D5/Tests/SimpleCodeCrackerTest.php
+++ src/D5/Tests/SimpleCodeCrackerTest.php
@@ -8,7 +8,7 @@ class SimpleCodeCrackerTest extends \PHPUnit_Framework_TestCase
 {
     public function test_code_cracker()
     {
-//        $cracker = new SimpleCodeCracker('abc');
-//        $this->assertEquals('18f47a30', $cracker->getCode());
+        $cracker = new SimpleCodeCracker('abc');
+        $this->assertEquals('18f47a30', $cracker->getCode());
     }
 }
